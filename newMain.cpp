83f4dba5298eed0ed85f270c82690a80c44d5891
pochoclos <andre.melendezc@unmsm.edu.pe>
2024-10-23 12:43:39 -0500

    cambios importantes

--- newMain.cpp
+++ newMain.cpp
@@ -3,35 +3,15 @@
 #include <conio.h>
 #include <vector>
 #include <string>
-
-//Headers
-#include "menu/gotoxy.h"
-#include "menu/menus.h"
+#include <filesystem>
 
 using namespace std;
 
 int main() {
-    system("CLS");
-    ListaPedidos listaPedidos;
-    listaPedidos=leerPedidosDesdeCSV("pedidos.csv");
-    /*ListaLibros listalibros;
-    listalibros = leerLibrosCSV("Libros.csv");*/
-    /*Lista listaUsuarios;
-    listaUsuarios = leerUsuariosCSV("usuarios.csv");*/
-    cout<<"GAAAAAA";
-    cout<<"PERRRRRRRRRRRRRA"<<endl;
+    filesystem::path current_path = filesystem::current_path();
 
-    // Ejemplo de cómo usar la función 'convertirFecha'
-Pedidos pedido;
-
-// Suponiendo que 'campo' contiene la fecha en formato string como "16/10/2024"
-string campo = "16/10/2024";
-pedido.fechaPedido = convertirFecha(campo);
-
-// Mostrar la fecha para verificar
-cout << "Dia: " << pedido.fechaPedido.dia << endl;
-cout << "Mes: " << pedido.fechaPedido.mes << endl;
-cout << "Ano: " << pedido.fechaPedido.año << endl;
+    // Imprimir el directorio de trabajo
+    cout << "Directorio de trabajo: " << current_path << endl;
    
 
 
