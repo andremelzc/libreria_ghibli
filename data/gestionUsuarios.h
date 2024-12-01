243042ef286e8715b9bc1809cb29e25db6af0ece
pochoclos <andre.melendezc@unmsm.edu.pe>
2024-11-30 20:52:20 -0500

    part 2

--- data/gestionUsuarios.h
+++ data/gestionUsuarios.h
@@ -19,7 +19,7 @@ Lista leerUsuariosCSV(string nombreArchivo);
 void guardar_CSV(Lista *lista, string nombreArchivo);
 
 // Registrar usuario (vista administrador)
-void menuInicio_registrarUsuario()
+void gestionUsuarios_registrarUsuario()
 {
     Lista *lista = new Lista();
     char respuesta[10];
